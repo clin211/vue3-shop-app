e9bb8f1986d113da85726251e045cbb99774f71f
forest-211 <767425412lin@gmail.com>
2024-07-02 22:44:01 +0800

    feat: 修改配置

--- src/App.vue
+++ src/App.vue
@@ -21,30 +21,27 @@ const isShowNav = ref(false);
 const userStore = useUserStore();
 const cartStore = useCartStore();
 const currentUser = lastLoginUser();
-console.log(currentUser);
 
 onMounted(async () => {
-  if (getToken()) {
-    userStore.setInfo(currentUser);
-    const res = await getUserInfo(currentUser);
-    console.log(res);
-  } else {
-    userStore.reset();
-  }
+    if (getToken()) {
+        userStore.setInfo(currentUser);
+        await getUserInfo(currentUser);
+    } else {
+        userStore.reset();
+    }
 });
 
 // 监视路由变动，然后控制TabBar组件的存在
 watch(route, newVal => {
-  isShowNav.value = newVal.meta.isShowNav;
+    isShowNav.value = newVal.meta.isShowNav;
 });
 </script>
 
 <template>
-  <RouterView />
-  <TabBar
-    v-if="isShowNav"
-    :cart-count="cartStore?.count ? cartStore.count : null"
-  />
+    <RouterView />
+    <TabBar
+        v-if="isShowNav"
+        :cart-count="cartStore?.count ? cartStore.count : null" />
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -6,35 +6,38 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 import progress from 'vite-plugin-progress';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/shop/',
-    plugins: [
-        vue(),
-        vueJsx(),
-        Components({
-            resolvers: [VantResolver()],
-        }),
-        progress(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-    build: {
-        outDir: 'shop',
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8199',
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/api/, ''),
-                logLevel: 'debug', // 启用详细日志
-                secure: false, //忽略安全证书
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            vue(),
+            vueJsx(),
+            Components({
+                resolvers: [VantResolver()],
+            }),
+            progress(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
-    },
+        build: {
+            outDir: 'shop',
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_REQUEST_URL,
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/api/, ''),
+                    logLevel: 'debug', // 启用详细日志
+                    secure: false, //忽略安全证书
+                },
+            },
+        },
+    };
 });
